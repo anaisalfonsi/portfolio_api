1d8470190340e20a624b5bb26d2f642bc2474c8e
anaisalfonsi <alfonsi_anais@hotmail.fr>
2022-10-10 23:20:27 +0200

    state processor issue fixed

--- src/State/UserProcessor.php
+++ src/State/UserProcessor.php
@@ -4,52 +4,34 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
  * @Annotation
  */
-class UserProcessor implements ProcessorInterface
+final class UserProcessor implements ProcessorInterface
 {
     private $decorated;
-    private $entityManager;
     private $passwordHasher;
 
-    public function __construct(ProcessorInterface $decorated, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    public function __construct(ProcessorInterface $decorated, UserPasswordHasherInterface $passwordHasher)
     {
         $this->decorated = $decorated;
-        $this->entityManager = $entityManager;
         $this->passwordHasher = $passwordHasher;
     }
 
-    public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []) : mixed
     {
-        $result = $this->decorated->process($data, $operation, $uriVariables, $context);
-        $this->persistPassword($data);
-        return $result;
-    }
-
-    /**
-     * @param User $user
-     */
-    private function persistPassword(User $user)
-    {
-        dd("je persiste le mot de passe");
-        die;
-        if ($user->getPlainPassword()) {
-            $user->setPassword(
+        if ($data->getPlainPassword()) {
+            $data->setPassword(
                 $this->passwordHasher->hashPassword(
-                    $user,
-                    $user->getPlainPassword()
+                    $data,
+                    $data->getPlainPassword()
                 )
             );
 
-            $user->eraseCredentials();
+            $data->eraseCredentials();
         }
-
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
+        return $this->decorated->process($data, $operation, $uriVariables, $context);
     }
 }
